a346535c402e863d04cb3149a0ccc7debe62aead
ayaantagkhan <a.khan021004@gmail.com>
2026-09-16 23:35:40 -0500

    Updated Arrays

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main() {
     vector.push(10);
     vector.push(20);
     vector.push(30);
-
+    std::cout << "Array Practice" << std::endl;
     std::cout << "Size: " << vector.getSize() << std::endl;
     std::cout << "Size: " << vector.getCapacity() << std::endl;
     std::cout << std::boolalpha;
@@ -19,7 +19,8 @@ int main() {
     std::cout << "Index 0: " << vector.at(0) << std::endl;
     std::cout << "Index 1: " << vector.at(1) << std::endl;
     std::cout << "Index 2: " << vector.at(2) << std::endl;
-
     std::cout << "Size: " << vector.getSize() << std::endl;
     return 0;
+
+    
 }
